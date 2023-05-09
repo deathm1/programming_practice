1affe7625347feeb3ed310dddf23e5dd43f1940c
de_bug <38889889+deathm1@users.noreply.github.com>
2023-05-09 05:20:20 +0000

    sql practice

---
+++ sql_practice/183. Customers Who Never Order/query.sql
@@ -0,0 +1,7 @@
+-- Write an SQL query to report all customers who never order anything.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+select c.Customers from Customers as C left join Orders as O on C.id = O.id where O.customerId is null;
