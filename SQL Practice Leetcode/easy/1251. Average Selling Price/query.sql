bb897acd98748ddfbd3dd9feead3aa5b7b169d80
de_bug <38889889+deathm1@users.noreply.github.com>
2023-05-17 16:02:27 +0000

    added a few more sql medium problems

---
+++ SQL Practice Leetcode/easy/1251. Average Selling Price/query.sql
@@ -0,0 +1,24 @@
+-- Write an SQL query to find the average selling 
+-- price for each product. average_price should be rounded to 2 decimal places.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+
+select 
+P.product_id,
+round(
+  (sum(
+    (P.price*1.00)*(US.units*1.00)
+  ) * 1.00) / (sum((US.units*1.00)) * 1.00) , 2
+) as 'average_price'
+from 
+Prices as P
+inner join 
+UnitsSold as US
+on
+P.product_id = US.product_id 
+where 
+US.purchase_date >= P.start_date and 
+US.purchase_date <= p.end_date group by P.product_id;
